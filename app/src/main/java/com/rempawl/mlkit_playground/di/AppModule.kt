3dbef04c1993cf76f02f4036549b99da02b50a44
rempawl <rem.pawl@gmail.com>
2024-12-03 15:12:43 +0100

    Improved performance by adding parallel detection execution with async
    Upgraded to kotlin 2.0

--- app/src/main/java/com/rempawl/mlkit_playground/di/AppModule.kt
+++ app/src/main/java/com/rempawl/mlkit_playground/di/AppModule.kt
@@ -4,23 +4,24 @@ import com.google.mlkit.vision.objects.ObjectDetector
 import com.google.mlkit.vision.text.TextRecognizer
 import com.rempawl.image.processing.CanvasProvider
 import com.rempawl.image.processing.ImageProcessingViewModel
-import com.rempawl.image.processing.usecase.ObjectDetectionUseCase
 import com.rempawl.image.processing.PaintProvider
-import com.rempawl.image.processing.usecase.TextDetectionUseCase
 import com.rempawl.image.processing.core.DispatchersProvider
+import com.rempawl.image.processing.usecase.ObjectDetectionUseCase
+import com.rempawl.image.processing.usecase.TextDetectionUseCase
 import com.rempawl.mlkit_playground.core.DispatchersProviderImpl
 import com.rempawl.mlkit_playground.di.MLKitProvider.provideObjectDetector
 import com.rempawl.mlkit_playground.di.MLKitProvider.provideTextRecognizer
 import org.koin.core.module.dsl.factoryOf
+import org.koin.core.module.dsl.singleOf
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val appModule = module {
-    factoryOf(::CanvasProvider)
-    factoryOf(::PaintProvider)
-    single<DispatchersProvider>() { DispatchersProviderImpl() }
-    single<ObjectDetector>() { provideObjectDetector() }
-    single<TextRecognizer>() { provideTextRecognizer() }
+    singleOf(::CanvasProvider)
+    singleOf(::PaintProvider)
+    singleOf<DispatchersProvider>(::DispatchersProviderImpl)
+    singleOf<ObjectDetector>(::provideObjectDetector)
+    singleOf<TextRecognizer>(::provideTextRecognizer)
     factoryOf(::ObjectDetectionUseCase)
     factoryOf(::TextDetectionUseCase)
     viewModelOf(::ImageProcessingViewModel)
